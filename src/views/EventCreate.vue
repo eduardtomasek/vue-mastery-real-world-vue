29a1066c050840c2b1b91ee9ba48424cdaa8615c
eduardtomasek <tomasek.e@gmail.com>
2020-07-15 22:21:33 +0200

    vuex mastery lesson 3

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  /*computed: {
+   
+    userName() {
+      return this.$store.state.user.name
+    },
+    userID() {
+      return this.$store.state.user.id
+    }
+  }*/
+  // computed: mapState({
+  /*
+    userName: state => state.user.name,
+    userID: state => state.user.id,
+    categories: state => state.categories
+    */
+  /*
+    user: 'user',
+    categories: 'categories'
+    */
+  // })
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped></style>

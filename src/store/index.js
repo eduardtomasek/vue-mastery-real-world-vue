86e4032f4e1970e26d482bf3a913f8471b9390b3
Eduard Tomasek <tomasek@organizetube.com>
2020-07-16 14:01:24 +0200

    Mastering Vuex Lesson 5

--- src/store/index.js
+++ src/store/index.js
@@ -16,11 +16,22 @@ export default new Vuex.Store({
       'food',
       'community'
     ],
-    events: []
+    events: [],
+    totalEventsCount: 0,
+    event: {}
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_TOTAL_EVENTS_COUNT(state, count) {
+      state.totalEventsCount = count
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     }
   },
   actions: {
@@ -28,12 +39,44 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit('SET_EVENTS', response.data)
+          commit(
+            'SET_TOTAL_EVENTS_COUNT',
+            response.headers['x-total-count']
+              ? parseInt(response.headers['x-total-count'], 10)
+              : 0
+          )
+        })
+        .catch(error => {
+          console.error(error.message)
+        })
+    },
+    fetchEvent({ commit, getters }, id) {
+      let event = getters.getEventById(id)
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.error(error.message)
+          })
+      }
     }
   },
   modules: {},
   getters: {
     catLength: state => {
       return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
     }
   }
 })
